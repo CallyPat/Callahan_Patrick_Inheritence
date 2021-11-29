9e7f6e5a044bf09cae3cbc376e3622f98cee63f8
CallyPat <patrick.callahan@sinclair.edu>
2021-11-29 18:14:07 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+// Patrick Callahan
+// Cis 1202
+// 11/29/2021
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+}
---
+++ Vehicle.h
@@ -0,0 +1,49 @@
+#pragma once
+// Patrick Callahan
+// Cis 1202
+// 11/29/2021
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+using namespace std;
+
+
+class Vehicle
+{
+
+string manufacturer;
+  
+
+int yearBuilt;
+  
+public:
+  
+
+Vehicle();
+  
+
+Vehicle(string x, int y);
+  
+
+string getMan();
+
+
+int getYear();
+  
+
+void setMan(string m);
+  
+
+void setYear(int y);
+  
+
+void displayInfo();
+};
+
+#endif
